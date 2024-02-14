458b83b8cc398d62f3a018da3ea6899567bb6fec
Karthika R <karthi.r1993@gmail.com>
2024-02-14 10:23:36 +0530

    create-react-app and tailwind CSS configuration

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-3xl font-bold">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Time to build Netflix GPT !😎
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
