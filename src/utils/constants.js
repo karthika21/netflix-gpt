80d97e5507cdff873f83e23d768dec954faea1db
Karthika R <karthi.r1993@gmail.com>
2024-02-15 13:26:22 +0530

    secondary container with movie list

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,8 @@ export const USER_LOGO =
 export const GITHUB_PROFILE =
   "https://avatars.githubusercontent.com/u/25986189?v=4";
 
+export const MOVIE_CDN_URL = "https://image.tmdb.org/t/p/w500/";
+
 export const API_OPTIONS = {
   method: "GET",
   headers: {
